d21faffbf94dcd43ff7e69a366bed52a70fdcef9
Kelerik <vincent.ng39@gmail.com>
2022-01-15 16:04:27 -0500

    server sync and get all categories route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,28 +1,41 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+router.get("/", (req, res) => {
+   // find all categories
+   Category.findAll({
+      // be sure to include its associated Products
+      include: [
+         {
+            model: Product,
+            attributes: ["product_name", "price", "stock"],
+         },
+      ],
+   })
+      .then((data) => res.json(data))
+      .catch((err) => {
+         console.log(err);
+         res.status(400).json(err);
+      });
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", (req, res) => {
+   // find one category by its `id` value
+   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+   // create a new category
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+   // delete a category by its `id` value
 });
 
 module.exports = router;
